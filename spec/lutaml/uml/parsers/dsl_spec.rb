fe76095e52e9a9fa05d0fe4cb9e4ee513190167e
Тараскин Михаил <MITaraskin@domclick.ru>
2020-08-20 17:54:28 +0300

    glossarist/concept-model#15 fixes for graphviz rendering with the new LutaML classes, new specs

--- spec/lutaml/uml/parsers/dsl_spec.rb
+++ spec/lutaml/uml/parsers/dsl_spec.rb
@@ -5,6 +5,15 @@ require "spec_helper"
 RSpec.describe Lutaml::Uml::Parsers::Dsl do
   describe ".parse" do
     subject(:parse) { described_class.parse(conent) }
+    subject(:format_parsed_document) do
+      Lutaml::Uml::Formatter::Graphviz.new.format_document(parse)
+    end
+
+    shared_examples 'the correct graphviz formatting' do
+      it 'does not raise error on graphviz formatting' do
+        expect { format_parsed_document }.to_not raise_error
+      end
+    end
 
     context "when simple diagram without attributes" do
       let(:conent) do
@@ -14,6 +23,8 @@ RSpec.describe Lutaml::Uml::Parsers::Dsl do
       it "creates Lutaml::Uml::Document object from supplied dsl" do
         expect(parse).to be_instance_of(Lutaml::Uml::Document)
       end
+
+      it_behaves_like 'the correct graphviz formatting'
     end
 
     context "when diagram with attributes" do
@@ -25,6 +36,8 @@ RSpec.describe Lutaml::Uml::Parsers::Dsl do
         expect(parse).to be_instance_of(Lutaml::Uml::Document)
         expect(parse.title).to eq("my diagram")
       end
+
+      it_behaves_like 'the correct graphviz formatting'
     end
 
     context "when multiply classes entries" do
@@ -36,6 +49,8 @@ RSpec.describe Lutaml::Uml::Parsers::Dsl do
         expect(parse).to be_instance_of(Lutaml::Uml::Document)
         expect(parse.classes.length).to eq(3)
       end
+
+      it_behaves_like 'the correct graphviz formatting'
     end
   end
 end
