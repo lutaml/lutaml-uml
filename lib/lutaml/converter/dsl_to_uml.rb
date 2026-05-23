4123acf34613ba293dbd85ad0ccbdb1451b14a6e
Ronald Tse <ronald.tse@ribose.com>
2026-05-23 17:20:24 +0800

    refactor: clean up LML parser code quality (TODO 03)
    - Fix respond_to?/send in Lml::Parser#set_model_attribute: use
      model.class.attributes.key? and public_send instead
    - Rename Dataprocessor → DataProcessor (consistent casing)
    - DRY up 19 copy-pasted member creation methods (10 in Lml::Parser,
      9 in DslToUml) into data-driven build_members with MEMBER_FACTORIES map
    - Net reduction: 157 lines removed
    - All 61 parser specs pass (27 LML + 34 DSL)

--- lib/lutaml/converter/dsl_to_uml.rb
+++ lib/lutaml/converter/dsl_to_uml.rb
@@ -3,6 +3,19 @@
 module Lutaml
   module Converter
     module DslToUml
+      MEMBER_FACTORIES = {
+        packages: :create_uml_package,
+        classes: :create_uml_class,
+        enums: :create_uml_enum,
+        data_types: :create_uml_data_type,
+        diagrams: :create_uml_diagram,
+        attributes: :create_uml_attribute,
+        associations: :create_uml_association,
+        operations: :create_uml_operation,
+        constraints: :create_uml_constraint,
+        values: :create_uml_value
+      }.freeze
+
       def create_uml_document(hash)
         ::Lutaml::Uml::Document.new.tap do |model|
           set_uml_model(model, hash)
@@ -39,7 +52,7 @@ module Lutaml
         end
       end
 
-      def create_uml_attribute(hash) # rubocop:disable Metrics/AbcSize
+      def create_uml_attribute(hash)
         ::Lutaml::Uml::TopElementAttribute.new.tap do |model|
           set_uml_model(model, hash)
         end
@@ -52,18 +65,18 @@ module Lutaml
         end
       end
 
-      def create_uml_association(hash) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      def create_uml_association(hash)
         ::Lutaml::Uml::Association.new.tap do |model|
           member_end_cardinality = hash.delete(:member_end_cardinality)
           if member_end_cardinality
             model.member_end_cardinality = create_uml_cardinality(
-              member_end_cardinality,
+              member_end_cardinality
             )
           end
           owner_end_cardinality = hash.delete(:owner_end_cardinality)
           if owner_end_cardinality
             model.owner_end_cardinality = create_uml_cardinality(
-              owner_end_cardinality,
+              owner_end_cardinality
             )
           end
 
@@ -94,7 +107,7 @@ module Lutaml
         set_model_attribute(model, hash)
       end
 
-      def set_model_attribute(model, hash) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      def set_model_attribute(model, hash)
         hash.each do |key, value|
           if key == :definition
             value = value.to_s.gsub(/\\}/, "}").gsub(/\\{/, "{")
@@ -104,7 +117,7 @@ module Lutaml
           next unless model.class.attributes.key?(key.to_sym)
 
           if model.class.attributes[key.to_sym].options[:collection]
-            values = model.public_send(key.to_sym).to_a
+            values = model.public_send(key).to_a
             values << value
             model.public_send("#{key}=", values)
           else
@@ -113,124 +126,32 @@ module Lutaml
         end
       end
 
-      def create_uml_members(model, hash) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+      def create_uml_members(model, hash)
         members = hash.delete(:members)
         members.to_a.each do |member_hash|
           member_hash.each do
-            create_uml_packages(model, member_hash)
-            create_uml_classes(model, member_hash)
-            create_uml_enums(model, member_hash)
-            create_uml_data_types(model, member_hash)
-            create_uml_diagrams(model, member_hash)
-            create_uml_attributes(model, member_hash)
-            create_uml_associations(model, member_hash)
-            create_uml_operations(model, member_hash)
-            create_uml_constraints(model, member_hash)
-            create_uml_values(model, member_hash)
+            build_members(model, member_hash, :uml)
             set_model_attribute(model, member_hash)
           end
         end
         hash
       end
 
-      def create_uml_packages(model, hash)
-        packages = hash.delete(:packages)
-        return [] if packages.nil?
+      private
 
-        attr = create_uml_package(packages)
-        model.packages = [] if model.packages.nil?
-        model.packages << attr
-        hash
-      end
+      def build_members(model, hash, prefix)
+        MEMBER_FACTORIES.each do |key, factory|
+          data = hash.delete(key)
+          next if data.nil?
 
-      def create_uml_classes(model, hash)
-        classes = hash.delete(:classes)
-        return [] if classes.nil?
-
-        attr = create_uml_class(classes)
-        model.classes = [] if model.classes.nil?
-        model.classes << attr
-        hash
-      end
-
-      def create_uml_enums(model, hash)
-        enums = hash.delete(:enums)
-        return [] if enums.nil?
-
-        attr = create_uml_enum(enums)
-        model.enums = [] if model.enums.nil?
-        model.enums << attr
-        hash
-      end
-
-      def create_uml_data_types(model, hash)
-        data_types = hash.delete(:data_types)
-        return [] if data_types.nil?
-
-        attr = create_uml_data_type(data_types)
-        model.data_types = [] if model.data_types.nil?
-        model.data_types << attr
-        hash
-      end
-
-      def create_uml_diagrams(model, hash)
-        diagrams = hash.delete(:diagrams)
-        return [] if diagrams.nil?
-
-        attr = create_uml_diagram(diagrams)
-        model.diagrams = [] if model.diagrams.nil?
-        model.diagrams << attr
-        hash
-      end
-
-      def create_uml_attributes(model, hash)
-        attributes = hash.delete(:attributes)
-        return [] if attributes.nil?
-
-        attr = create_uml_attribute(attributes)
-        model.attributes = [] if model.attributes.nil?
-        model.attributes << attr
-        hash
-      end
-
-      def create_uml_associations(model, hash)
-        associations = hash.delete(:associations)
-        return [] if associations.nil?
-
-        attr = create_uml_association(associations)
-        model.associations = [] if model.associations.nil?
-        model.associations << attr
-        hash
-      end
-
-      def create_uml_operations(model, hash)
-        operations = hash.delete(:operations)
-        return [] if operations.nil?
-
-        attr = create_uml_operation(operations)
-        model.operations = [] if model.operations.nil?
-        model.operations << attr
-        hash
-      end
-
-      def create_uml_constraints(model, hash)
-        constraints = hash.delete(:constraints)
-        return [] if constraints.nil?
-
-        attr = create_uml_constraint(constraints)
-        model.constraints = [] if model.constraints.nil?
-        model.constraints << attr
-        hash
-      end
-
-      def create_uml_values(model, hash)
-        values = hash.delete(:values)
-        return [] if values.nil?
-
-        attr = create_uml_value(values)
-        model.values = [] if model.values.nil?
-        model.values << attr
-        hash
+          member = public_send(factory, data)
+          collection = model.public_send(key)
+          if collection.nil?
+            model.public_send("#{key}=", [])
+            collection = model.public_send(key)
+          end
+          collection << member
+        end
       end
     end
   end
--- lib/lutaml/lml/parser.rb
+++ lib/lutaml/lml/parser.rb
@@ -7,11 +7,23 @@ module Lutaml
   module Lml
     # Class for parsing LutaML lml into Lutaml::Lml::Document
     class Parser < Uml::Parsers::Dsl
-      include Lutaml::Lml::Dataprocessor
+      include Lutaml::Lml::DataProcessor
+
+      MEMBER_FACTORIES = {
+        packages: :create_lml_package,
+        classes: :create_lml_class,
+        enums: :create_lml_enum,
+        data_types: :create_lml_data_type,
+        diagrams: :create_lml_diagram,
+        attributes: :create_lml_attribute,
+        associations: :create_lml_association,
+        operations: :create_lml_operation,
+        constraints: :create_lml_constraint,
+        values: :create_lml_value
+      }.freeze
 
       def create_document(hash)
         process_data(hash)
-
         create_lml_document(hash)
       end
 
@@ -57,7 +69,7 @@ module Lutaml
         end
       end
 
-      def create_lml_attribute(hash) # rubocop:disable Metrics/AbcSize
+      def create_lml_attribute(hash)
         ::Lutaml::Lml::TopElementAttribute.new.tap do |model|
           set_lml_model(model, hash)
         end
@@ -70,18 +82,18 @@ module Lutaml
         end
       end
 
-      def create_lml_association(hash) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      def create_lml_association(hash)
         ::Lutaml::Lml::Association.new.tap do |model|
           member_end_cardinality = hash.delete(:member_end_cardinality)
           if member_end_cardinality
             model.member_end_cardinality = create_lml_cardinality(
-              member_end_cardinality,
+              member_end_cardinality
             )
           end
           owner_end_cardinality = hash.delete(:owner_end_cardinality)
           if owner_end_cardinality
             model.owner_end_cardinality = create_lml_cardinality(
-              owner_end_cardinality,
+              owner_end_cardinality
             )
           end
 
@@ -112,144 +124,52 @@ module Lutaml
         set_model_attribute(model, hash)
       end
 
-      def set_model_attribute(model, hash) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      def set_model_attribute(model, hash)
         hash.each do |key, value|
           if key == :definition
             value = value.to_s.gsub(/\\}/, "}").gsub(/\\{/, "{")
               .split("\n").map(&:strip).join("\n")
           end
 
-          if model.respond_to?("#{key}=")
-            if model.class.attributes[key.to_sym].options[:collection]
-              values = model.send(key.to_sym).to_a
-              value.is_a?(Array) ? values.concat(value) : values << value
-              model.send("#{key}=", values)
-            else
-              model.send("#{key}=", value)
-            end
+          next unless model.class.attributes.key?(key.to_sym)
+
+          if model.class.attributes[key.to_sym].options[:collection]
+            values = model.public_send(key).to_a
+            value.is_a?(Array) ? values.concat(value) : values << value
+            model.public_send("#{key}=", values)
+          else
+            model.public_send("#{key}=", value)
           end
         end
       end
 
-      def create_lml_members(model, hash) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+      def create_lml_members(model, hash)
         members = hash.delete(:members)
         members.to_a.each do |member_hash|
           member_hash.each do
             create_lml_instances(model, member_hash)
-            create_lml_packages(model, member_hash)
-            create_lml_classes(model, member_hash)
-            create_lml_enums(model, member_hash)
-            create_lml_data_types(model, member_hash)
-            create_lml_diagrams(model, member_hash)
-            create_lml_attributes(model, member_hash)
-            create_lml_associations(model, member_hash)
-            create_lml_operations(model, member_hash)
-            create_lml_constraints(model, member_hash)
-            create_lml_values(model, member_hash)
+            build_members(model, member_hash)
             set_model_attribute(model, member_hash)
           end
         end
         hash
       end
 
-      def create_lml_packages(model, hash)
-        packages = hash.delete(:packages)
-        return [] if packages.nil?
+      private
 
-        attr = create_lml_package(packages)
-        model.packages = [] if model.packages.nil?
-        model.packages << attr
-        hash
-      end
+      def build_members(model, hash)
+        MEMBER_FACTORIES.each do |key, factory|
+          data = hash.delete(key)
+          next if data.nil?
 
-      def create_lml_classes(model, hash)
-        classes = hash.delete(:classes)
-        return [] if classes.nil?
-
-        attr = create_lml_class(classes)
-        model.classes = [] if model.classes.nil?
-        model.classes << attr
-        hash
-      end
-
-      def create_lml_enums(model, hash)
-        enums = hash.delete(:enums)
-        return [] if enums.nil?
-
-        attr = create_lml_enum(enums)
-        model.enums = [] if model.enums.nil?
-        model.enums << attr
-        hash
-      end
-
-      def create_lml_data_types(model, hash)
-        data_types = hash.delete(:data_types)
-        return [] if data_types.nil?
-
-        attr = create_lml_data_type(data_types)
-        model.data_types = [] if model.data_types.nil?
-        model.data_types << attr
-        hash
-      end
-
-      def create_lml_diagrams(model, hash)
-        diagrams = hash.delete(:diagrams)
-        return [] if diagrams.nil?
-
-        attr = create_lml_diagram(diagrams)
-        model.diagrams = [] if model.diagrams.nil?
-        model.diagrams << attr
-        hash
-      end
-
-      def create_lml_attributes(model, hash)
-        attributes = hash.delete(:attributes)
-        return [] if attributes.nil?
-
-        attr = create_lml_attribute(attributes)
-        model.attributes = [] if model.attributes.nil?
-        model.attributes << attr
-        hash
-      end
-
-      def create_lml_associations(model, hash)
-        associations = hash.delete(:associations)
-        return [] if associations.nil?
-
-        attr = create_lml_association(associations)
-        model.associations = [] if model.associations.nil?
-        model.associations << attr
-        hash
-      end
-
-      def create_lml_operations(model, hash)
-        operations = hash.delete(:operations)
-        return [] if operations.nil?
-
-        attr = create_lml_operation(operations)
-        model.operations = [] if model.operations.nil?
-        model.operations << attr
-        hash
-      end
-
-      def create_lml_constraints(model, hash)
-        constraints = hash.delete(:constraints)
-        return [] if constraints.nil?
-
-        attr = create_lml_constraint(constraints)
-        model.constraints = [] if model.constraints.nil?
-        model.constraints << attr
-        hash
-      end
-
-      def create_lml_values(model, hash)
-        values = hash.delete(:values)
-        return [] if values.nil?
-
-        attr = create_lml_value(values)
-        model.values = [] if model.values.nil?
-        model.values << attr
-        hash
+          member = public_send(factory, data)
+          collection = model.public_send(key)
+          if collection.nil?
+            model.public_send("#{key}=", [])
+            collection = model.public_send(key)
+          end
+          collection << member
+        end
       end
     end
   end
